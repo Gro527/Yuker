1e82e8e2565384843263e91f0e54126d6ea54cd8
croyance <1225280899@qq.com>
2019-02-06 15:58:04 +0800

    增加后续页面
    1、增加三种推荐方案，但有swiper问题未解决
    2、最终推荐方案页面不完善

--- pages/index/plan1/plan1_1/plan1_1.js
+++ pages/index/plan1/plan1_1/plan1_1.js
@@ -1,66 +1,79 @@
-// pages/index/plan1/plan1_1/plan1_1.js
+
+//index.js
+
+//获取应用实例
+
+var app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    /**
 
+        * 页面配置
+
+        */
+    winWidth: 0,
+    winHeight: 0,
+    // tab切换
+    currentTab: 0,
+  },
+
+  onLoad: function () {
+    var that = this;
+    /**
+
+     * 获取系统信息
+
+     */
+    wx.getSystemInfo({
+
+      success: function (res) {
+        that.setData({
+          winWidth: res.windowWidth,
+          winHeight: res.windowHeight
+        });
+      }
+    });
+
+  },
+  /**
+
+     * 滑动切换tab
+
+     */
+
+  bindChange: function (e) {
+    var that = this;
+    that.setData({ currentTab: e.detail.current });
   },
 
   /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
 
+   * 点击tab切换
+
+   */
+
+  swichNav: function (e) {
+    var that = this;
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current
+      })
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  daohang:function(e){
+   wx.showToast({
+    icon: 'none',
+    title: '高德地图导航'
+  });
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  final:function(e){
+    wx.navigateTo({
+      url: '/pages/index/plan1/plan1_final/plan1_final',
+    })
 
   }
 })
--- pages/index/plan1/plan1_final/plan1_final.js
+++ pages/index/plan1/plan1_final/plan1_final.js
@@ -1,74 +1,72 @@
-// pages/index/index-yuker/index-yuker.js
+// pages/index/plan1/plan1_final/plan1_final.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-     this.setData({
-         
-        })  
-  },
 
-  plan1:function(){
-    wx.navigateTo({
-      url: '/pages/index/plan1/plan1',
-    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
+  },
+
+  return:function(e){
+    wx.reLaunch({
+        url: '/pages/index/index',
+    })
   }
 })
